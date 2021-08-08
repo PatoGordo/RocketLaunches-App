906fdaaf09a23558f762285db4dcf6ae6cb916a9
FlutLab.io <>
2021-08-08 22:15:30 +0000

    Autosave

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../widgets/cards.rocket_launch_card.dart';
+import '../widgets/cards/rocket_launch_card.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
