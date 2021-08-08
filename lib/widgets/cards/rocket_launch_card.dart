fa805d0085afcf49377bff70122923326c4f0fdd
FlutLab.io <>
2021-08-08 22:29:02 +0000

    Autosave

--- lib/widgets/cards/rocket_launch_card.dart
+++ lib/widgets/cards/rocket_launch_card.dart
@@ -28,7 +28,7 @@ class RocketLaunchCard extends StatelessWidget {
           Container(
             child: Row(
               children: <Widget>[
-                Icon(Ionicons.rocket),
+                Icon(Ionicons.rocket_sharp),
               ],
             ),
           ),
