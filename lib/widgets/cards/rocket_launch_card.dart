b7c14abacdeec2af7c16265345ac6001327f710d
FlutLab.io <>
2021-08-08 22:34:38 +0000

    Autosave

--- lib/widgets/cards/rocket_launch_card.dart
+++ lib/widgets/cards/rocket_launch_card.dart
@@ -31,8 +31,8 @@ class RocketLaunchCard extends StatelessWidget {
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
-                topLeft: BorderRadius.circular(10),
-                topRight: BorderRadius.circular(10),
+                topLeft: BorderRadius.all(10),
+                topRight: BorderRadius.all(10),
               ),
             ),
             child: Row(
