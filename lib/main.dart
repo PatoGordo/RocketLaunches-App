9033d29d4c6fe1afde7271713c97435da2c56078
FlutLab.io <>
2021-08-08 22:40:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() {
       title: 'No Horny',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primaryColor: Colors.white,
         scaffoldBackgroundColor: Color(0xff121212),
       ),
       routes: {
