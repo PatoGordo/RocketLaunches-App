8180bd3fc92198961a25ff96479164f6d07a35e0
FlutLab.io <>
2021-08-08 23:01:32 +0000

    Autosave

--- lib/widgets/cards/rocket_launch_card.dart
+++ lib/widgets/cards/rocket_launch_card.dart
@@ -49,11 +49,11 @@ class RocketLaunchCard extends StatelessWidget {
                 Column(children: <Widget>[
                   Text(
                     'Rocket name - Manufacturer',
-                    textAlign: TextAlign.start,
+                    textAlign: TextAlign.left,
                   ),
                   Text(
                     '00/00/0000, 00:00:00 PM/AM GMT+/- 0',
-                    textAlign: TextAlign.start,
+                    textAlign: TextAlign.left,
                   )
                 ])
               ],
