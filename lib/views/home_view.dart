3db728b4c813427153cc7bab54ccee485385146b
FlutLab.io <>
2021-08-08 22:02:22 +0000

    Autosave

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -31,12 +31,14 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: <Widget>[
-          Text(
-            "Next Rocket Launch",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
+          Center(
+            child: Text(
+              "Next Rocket Launch",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
             ),
           ),
         ],
