a4fdaa32c4db1c004243ef771dc415c746455bf5
FlutLab.io <>
2021-08-08 22:11:56 +0000

    Autosave

---
+++ lib/widgets/cards/rocket_launch_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class RocketLaunchCard extends StatelessWidget {
+  RocketLaunchCard(
+    this.rocketName,
+    this.rocketManufacturer,
+    this.launchDate,
+    this.missionName,
+    this.missionDescription,
+  );
+
+  String rocketName;
+  String rocketManufacturer;
+  String launchDate;
+  String missionName;
+  String missionDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: <Widget>[
+                // change icon to a rocket icon
+                Icon(Icons.launch),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
